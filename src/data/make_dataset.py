ea9814d83926b2a0a373c4b176969e96f403dcf7
Anurodh Mohapatra <anurodhmohapatra@gmail.com>
2021-08-12 21:14:53 +0530

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,7 +6,7 @@ from dotenv import find_dotenv, load_dotenv
 import make_csv
 
 
-def main(input_filepath, output_filepath):
+def main(input_filepath='.', output_filepath='.'):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
